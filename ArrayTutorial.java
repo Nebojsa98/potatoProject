17e4fe9ee16766c4907996cff87cf218a978d71f
Nebojsa98 <nebojsa.sim2@gmail.com>
2015-10-17 23:36:48 +0200

    Added tutorial

---
+++ ArrayTutorial.java
@@ -0,0 +1,17 @@
+public class ArrayTutorial{
+
+static  int[] arrayNew = new int[10];
+
+public static void main(String[] args){
+
+    for(int i = 0; i<=(arrayNew.length)-1;i++){
+
+      arrayNew[i]=i;
+    }
+    for(int i = 0; i<=arrayNew.length-1;i++){
+
+      System.out.println(arrayNew[i]);
+    }
+
+  }
+}
